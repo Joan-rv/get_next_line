384116064ce79c00e66558279e7b6884d636ea40
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-11-22 13:09:25 +0100

    Add code to facilitate testing

--- get_next_line.c
+++ get_next_line.c
@@ -64,14 +64,22 @@ char* get_next_line(int fd) {
     }
 }
 
-int main() {
-    // int fd = open("test.txt", O_RDONLY);
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        return -1;
+    }
+    int fd;
+    if (strcmp(argv[1], "stdin") == 0) {
+        fd = 0;
+    } else {
+        fd = open(argv[1], O_RDONLY);
+    }
     char* line;
-    while ((line = get_next_line(0))) {
+    while ((line = get_next_line(fd))) {
         printf("%s", line);
         fflush(stdout);
         free(line);
     }
-    // close(fd);
+    close(fd);
     return 0;
 }
