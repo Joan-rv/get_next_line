30264f93eb09d6104078a1b0ecdbb787aec42892
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-12-06 20:03:16 +0100

    Use realloc instead of malloc and memcpy

--- get_next_line.c
+++ get_next_line.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 1
 #endif
 
 size_t min(size_t a, size_t b) {
@@ -16,16 +16,12 @@ size_t min(size_t a, size_t b) {
     }
 }
 
-void* memresize(void* buf, size_t old_size, size_t size) {
-    void* new_buf = NULL;
-    if (size != 0) {
-        new_buf = malloc(size);
-        memcpy(new_buf, buf, min(size, old_size));
-    }
-    if (old_size != 0) {
+void* memresize(void* buf, size_t size) {
+    if (size == 0) {
         free(buf);
+        return NULL;
     }
-    return new_buf;
+    return realloc(buf, size);
 }
 
 char* remove_as_string(char** buf, size_t* size, size_t pos) {
@@ -35,10 +31,9 @@ char* remove_as_string(char** buf, size_t* size, size_t pos) {
     char* tmp = malloc((pos + 2) * sizeof(char));
     memcpy(tmp, *buf, (pos + 1) * sizeof(char));
     tmp[pos + 1] = '\0';
-    size_t old_size = *size;
     *size -= (pos + 1) * sizeof(char);
     memmove(*buf, *buf + (pos + 1), *size);
-    *buf = memresize(*buf, old_size, *size);
+    *buf = memresize(*buf, *size);
     return tmp;
 }
 
@@ -55,16 +50,17 @@ char* get_next_line(int fd) {
         if (done_reading) {
             return remove_as_string(&buf, &size, size - 1);
         }
-        buf = memresize(buf, size, size + BUFFER_SIZE);
+        buf = memresize(buf, size + BUFFER_SIZE);
         ssize_t read_size = read(fd, buf + size, BUFFER_SIZE);
         if (read_size < BUFFER_SIZE) {
             if (read_size < 0) {
-                free(buf);
+                buf = memresize(buf, 0);
+                size = 0;
                 return NULL;
             }
             done_reading = true;
+            buf = memresize(buf, size + read_size);
         }
-        buf = memresize(buf, size + BUFFER_SIZE, size + read_size);
         size += read_size;
     }
 }
